2082fbb53e6de5fe03f82839ec5fb83ded1a5a46
Wihtori Torkkeli <awito001@edu.xamk.fi>
2022-11-29 22:36:24 +0200

    ei etene

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Start from './components/Start';
+import { Button } from '@mui/material/';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Start/>
     </div>
   );
 }
---
+++ src/components/Start.js
@@ -0,0 +1,12 @@
+import {Button} from "@mui/material"
+
+function Start(){
+    
+    return(
+        <div>
+            För helvetten
+        </div>
+    );
+}
+
+export default Start;
