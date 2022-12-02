ed85a1e660e5d3f5c4e8a0e04ad19cc914499954
Wihtori Torkkeli <awito001@edu.xamk.fi>
2022-12-02 20:35:03 +0200

    nyt jo kuvat tulostuu

--- src/App.js
+++ src/App.js
@@ -6,15 +6,28 @@ import { useEffect, useState } from 'react';
 
 function App() {
   let [showStart, setShowStart] = useState(true);
-  
+  let [categories, setCategories] = useState([]);
+
+
   const hideStart = () =>{
     setShowStart(false);
   };
 
+  const searchByName = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+
+  async function apiCall(url){
+      const result = await fetch(url);
+      result.json().then(data => {
+          setCategories(data.drinks)
+      })
+  }
+
+  useEffect(()=>{apiCall(searchByName);},[]);
+
   return (
     <div className="App">
       {showStart? <Start hideStart={hideStart}/> : null}
-      {showStart? null: <Drinks/>}
+      {showStart? null: <Drinks categories={categories}/>}
     </div>
   );
 }
--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,9 +1,28 @@
-import { Typography, Grid, FormControl, InputLabel, Select } from "@mui/material";
+import { Typography, Grid, FormControl, InputLabel, Select, MenuItem, ImageList, ImageListItem, ListSubheader, ImageListItemBar } from "@mui/material";
 import { Container } from "@mui/system";
+import { useState } from "react";
 import './drinks.css';
 
 function Drinks(props){
+    let [selected, setSelected] = useState("");
+    let [drinks, setDrinks] = useState([]);
     
+    let [categories, setCategories] = useState(props.categories);
+
+    const requestNew = async(end) =>{
+        const result = await fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c="+end);
+        result.json().then(data => {
+            console.log(data.drinks)
+            setDrinks(data.drinks);
+        });
+    };
+
+    const handleChange = (category) => {
+        setSelected(category);
+        console.log(selected);
+    };
+
+
     return(
         <Container className="textIn drinkContainer" >
             <Grid container spacing={1}>
@@ -18,14 +37,27 @@ function Drinks(props){
                                 label="Drink category"
                                 className="select-drink-category"
                                 variant="outlined"
+                                value={selected}
                             >
-
+                                {categories.map((category) => (
+                                    <MenuItem value={category.strCategory} key={category.strCategory} onClick={()=>{handleChange(category.strCategory); requestNew(category.strCategory)}}>{category.strCategory}</MenuItem>
+                                ))}
                             </Select>
                         </FormControl>
                     </form>
                 </Grid>
                 <Grid item xs={12} md={12} lg={8} className="grid-item">
-                    <Typography variant="h6">Here are some drink suggestions:</Typography>
+                    <ImageList cols={5}>
+                        <ImageListItem key="Subheader" cols={5}>
+                            <ListSubheader component="div">Suggestions:</ListSubheader>
+                        </ImageListItem>
+                        {drinks.slice(0,10).map((drink)=>(
+                            <ImageListItem key={drink.strDrink}>
+                                <img src={`${drink.strDrinkThumb}`} width={50}/>
+                                <ImageListItemBar title={drink.strDrink}/>
+                            </ImageListItem>
+                        ))}
+                    </ImageList>
                 </Grid>
             </Grid>
         </Container>
